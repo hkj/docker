955b6e46d0d2199447f81d865706d3ea4799faec
Yasuhisa Kita <hkjkyoto@gmail.com>
2018-11-11 17:09:18 +0900

    add lv command

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ apt -y install \
    apt-transport-https \
    ca-certificates \
    curl \
+   lv \
    software-properties-common
 # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
@@ -16,4 +17,7 @@ apt -y install docker-ce
 curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 # apt install python3
-# apt install pip3
+apt install -y python3-pip
+#
+# pytorch for python3.6, pip, CUDA9.2
+# pip3 install http://download.pytorch.org/whl/cu92/torch-0.4.1-cp36-cp36m-linux_x86_64.whl
