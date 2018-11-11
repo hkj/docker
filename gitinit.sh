49e0f8c2e50cb8c0e6703f174da92554cbc74d40
Yasuhisa Kita <hkjkyoto@gmail.com>
2018-11-11 16:40:22 +0900

    remove sudo

--- gitinit.sh
+++ gitinit.sh
@@ -4,7 +4,8 @@ apt -y install \
    ca-certificates \
    curl \
    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
